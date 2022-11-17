30ecd20936c8b95e1baf9b3a9f311762acbddc72
x3pi <45082550+x3pi@users.noreply.github.com>
2022-11-17 19:43:25 +0700

    edit port

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
